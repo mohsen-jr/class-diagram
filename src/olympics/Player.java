5565187ef4eed751b49805b7ef505b97ad33049e
mahmoud_mohsen <mahmoud_mohsen@192.168.197.1>
2021-05-09 05:21:20 +0200

    Initial commit.

--- src/olympics/Player.java
+++ src/olympics/Player.java
@@ -15,13 +15,7 @@ public class Player extends Person {
     private String coachName;
     private String team;
     
-    public Player(int player_id){
-        playerID = player_id;
-    }
-//    
-//    public Player(){
-//        playerID = 0;
-//    }
+    
     public void setID(int id){
         this.playerID = id;
     }
